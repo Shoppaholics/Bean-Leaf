8455f20ba26d85e4f132a67ffb604b6941f8ed2c
amitha2210 <amithavuputuri@yahoo.com.sg>
2025-01-18 15:26:05 +0800

    Add code for mapbox

---
+++ app/(tabs)/map.tsx
@@ -0,0 +1,13 @@
+import MapboxMap from "@/components/MapboxMap";
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Drink Ratings</h1>
+      <MapboxMap />
+    </div>
+  );
+};
+
+export default App;
